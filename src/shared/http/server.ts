dc4d95f65aa242b71f80b2b4034b6efb416b8b05
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-01-30 20:52:13 -0300

    chore: adicionando validação de parametros com celebrate

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
-import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
+
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
+import { errors } from 'celebrate';
 
 import routes from './routes';
 import AppError from './errors/AppError';
@@ -14,6 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+app.use(errors());
 
 app.use(
   (error: Error, request: Request, response: Response, next: NextFunction) => {
