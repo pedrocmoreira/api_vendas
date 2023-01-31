7ddc14c7f49b08b12dc25b8e7707715eb4f95cce
Pedro Moreira <pedrocmoreira97@gmail.com>
2023-01-31 04:41:09 -0300

    chore: configurando id user em isAuthenticated

--- src/modules/users/middlewares/isAuthenticated.ts
+++ src/modules/users/middlewares/isAuthenticated.ts
@@ -5,6 +5,12 @@ import AppError from '@shared/http/errors/AppError';
 
 import authConfig from '@config/auth';
 
+interface TokenPayLoad {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
 export default function isAuthenticated(
   request: Request,
   response: Response,
@@ -21,6 +27,13 @@ export default function isAuthenticated(
 
   try {
     const decodeToken = verify(token, authConfig.jwt.secret);
+
+    const { sub } = decodeToken as TokenPayLoad;
+
+    request.user = {
+      id: sub,
+    };
+
     return next();
   } catch (error) {
     throw new AppError('Invalid JWT Token.');
